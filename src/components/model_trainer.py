03652f9f00434fc33825e8f4122d8da2e224d454
Shubham Chandra <shubhamchandrawork@gmail.com>
2024-06-03 17:07:35 -0400

    Hyper Parameter Tunned Model

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
 from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -42,9 +42,113 @@ class ModelTrainer:
                 "XGB Regressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(),
                 "AdaBoost Regressor": AdaBoostRegressor(),
+                'Ridge Regression': Ridge(),
+                'Lasso Regression': Lasso()
             }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+
+            params_old ={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor": {},
+                'XGB Regressor': {
+                    'n_estimators': [100, 200]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                'Ridge Regression': {'alpha': [0.01, 0.1, 1, 10, 100]},
+                'Lasso Regression': {'alpha': [0.01, 0.1, 1, 10, 100]}
+                
+            }
+
+
+            params_new = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_depth': [None, 10, 20, 30, 40, 50],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4],
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "Random Forest": {
+                    'n_estimators': [100, 200, 300, 400, 500],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_depth': [None, 10, 20, 30, 40, 50],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4],
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                    'bootstrap': [True, False],
+                },
+                "Gradient Boosting": {
+                    'loss': ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'n_estimators': [100, 200, 300],
+                    'subsample': [0.7, 0.8, 0.9, 1.0],
+                    'criterion': ['friedman_mse', 'squared_error'],
+                    'max_depth': [3, 4, 5, 6],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4],
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    'leaf_size': [10, 20, 30, 40],
+                    'p': [1, 2],
+                },
+                "XGB Regressor": {
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'n_estimators': [100, 200, 300],
+                    'max_depth': [3, 4, 5, 6],
+                    'min_child_weight': [1, 3, 5],
+                    'subsample': [0.7, 0.8, 0.9, 1.0],
+                    'colsample_bytree': [0.7, 0.8, 0.9, 1.0],
+                },
+                "CatBoosting Regressor": {
+                    'depth': [4, 6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [100, 200, 300],
+                },
+                "AdaBoost Regressor": {
+                    'n_estimators': [50, 100, 200, 300],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'loss': ['linear', 'square', 'exponential'],
+                },
+                'Ridge Regression': {'alpha': [0.01, 0.1, 1, 10, 100]},
+                'Lasso Regression': {'alpha': [0.01, 0.1, 1, 10, 100]}
+            }
+
+
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params_old)
 
             best_model_score = max(model_report.values())
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
@@ -63,7 +167,7 @@ class ModelTrainer:
             predicted = best_model.predict(X_test)
             r2 = r2_score(y_test, predicted)
 
-            return r2
+            return (r2, best_model_name)
 
         except Exception as e:
             raise CustomException(e, sys)
